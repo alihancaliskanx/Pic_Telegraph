297dfe15b1dc101053692ec81ebc2b3fbbc9e633
alihancaliskanx <alihancaliskan@workmail.com>
2025-12-20 16:16:09 +0300

    update for temp

---
+++ slave/build/CMakeFiles/TelgrafApp.dir/TelgrafApp_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/TelgrafApp.dir/TelgrafApp_autogen/mocs_compilation.cpp.o: \
+ /home/sups/Documents/Code/mikroproje/Pic_Telegraph/slave/build/TelgrafApp_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
